3cd266f662132076ee666de4ed357b893e0e8de1
wi3li <sabriina.silva91@gmail.com>
2025-09-10 20:12:36 -0300

    corrigindo erros de dependencias

--- front-end/homepage/next.config.mjs
+++ front-end/homepage/next.config.mjs
@@ -6,9 +6,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    optimizeCss: true,
-  },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
